0d46647ccee5daa9655f3aba40fadef3edc92d1d
Will <Willi@BANDBOOK>
2024-11-07 08:41:40 -0500

    Initialized Visual Studio Project

---
+++ Lab Problem 9.2/9.2.cpp
@@ -0,0 +1,17 @@
+// File Name:     Lab Problem 9.2
+// Author:        William Inkrott
+// Date:          11/07/24
+// Description:   Check to see if an array is increasing or decreasing and if there are any duplicates present. 
+
+#include <iostream>
+#include <cmath>
+#include <string>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+int main(void)
+{
+
+}
